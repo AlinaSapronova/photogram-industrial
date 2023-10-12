ddd1f6bf5f889995137ce57898648fcf5b98a5dd
Alina Sapronova <106851105+AlinaSapronova@users.noreply.github.com>
2023-10-12 20:42:41 +0000

    Created associations for all classes

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,6 +27,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :own_photos, class_name: "Photo", foreign_key: "owner_id"
-  has_many :comments, foreign_key: "author_id"
+  has_many :own_photos, class_name: "Photo", foreign_key: :owner_id
+  has_many :comments, foreign_key: :author_id
+  has_many :sent_follow_requests, foreign_key: :sender_id, class_name: "FollowRequest"
+  has_many :received_follow_requests, foreign_key: :recipient_id, class_name: "FollowRequest"
+
+
 end
