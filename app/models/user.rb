f09d8dd6de285633896b69a8459f46eaf36f7f89
Alina Sapronova <106851105+AlinaSapronova@users.noreply.github.com>
2023-10-12 22:01:41 +0000

    will need to fix sample data

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,9 +41,9 @@ class User < ApplicationRecord
 has_many :liked_photos, through: :likes, source: :photos
 
 
-has_many :accepted_sent_follow_requests, -> {where(status: "accepted")}, foreign_key: :sender_id, class_name: "FollowRequest"
+has_many :accepted_sent_follow_requests, -> { accepted }, foreign_key: :sender_id, class_name: "FollowRequest"
 
-has_many :accepted_received_follow_requests, -> {where(status: "accepted")}, foreign_key: :recipient_id, class_name: "FollowRequest"
+has_many :accepted_received_follow_requests, -> { accepted }, foreign_key: :recipient_id, class_name: "FollowRequest"
 
 has_many :leaders, through: :accepted_sent_follow_requests, source: :recipient
 
